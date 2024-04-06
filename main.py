de4b48c745ca13b36f6f569eb3eb8402b75ea50c
Servio Quezada <inazumaheart@gmail.com>
2024-04-05 21:46:56 -0400

    new and update product route added

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 from fastapi.responses import HTMLResponse
 
-import supabase_client
+# from json import dumps,loads
+
+from supabase_config import supabase_client
 
 app = FastAPI()
+
+class Product(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    category: int | None = None
+    userid: str | None = None
+
+class Category(BaseModel):
+    name: str
+    slug: str
+    parent_category:  int | None = None
 
 @app.get("/", response_class=HTMLResponse)
 async def home_page():
@@ -22,12 +37,22 @@ async def home_page():
 
 @app.get("/products")
 def read_products():
-    response = supabase_client.table('products').select("*, categories(name,slug)").execute()
+    response = supabase_client.table('products').select("*,categories(name,slug)").execute()
     return response.data
 
 @app.get("/products/{item_id}")
 def read_product(item_id: int, q: Union[str, None] = None):
-    response = supabase_client.table('products').select("*, categories(name,slug)").eq('id', item_id).execute()
+    response = supabase_client.table('products').select("*,categories(name,slug)").eq('id', item_id).execute()
+    return response.data
+
+@app.post("/products")
+def create_product(product: Product):
+    response = supabase_client.table('products').insert(product).execute()
+    return response.data
+
+@app.put("/products/{item_id}")
+def update_product(item_id: int, product: Product):
+    response = supabase_client.table('products').update(dict(product)).eq("id",item_id).execute()
     return response.data
 
 @app.get("/categories")
@@ -38,6 +63,16 @@ def read_categories():
 @app.get("/categories/{item_id}")
 def read_categories(item_id: int, q: Union[str, None] = None):
     response = supabase_client.table('categories').select("*,categories(name,slug)").eq('id', item_id).execute()
+    return response.data
+
+@app.post("/categories")
+def create_categories(category: Category):
+    response = supabase_client.table('categories').insert(category).execute()
+    return response.data
+
+@app.put("/categories/{item_id}")
+def update_categories(item_id: int, category: Category):
+    response = supabase_client.table('categories').update(category).eq("id",item_id).execute()
     return response.data
 
 @app.get("/users")
--- supabase_config.py
+++ supabase_config.py
@@ -3,6 +3,7 @@ from supabase import create_client, Client
 from dotenv import load_dotenv
 
 load_dotenv()
+
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase_client: Client = create_client(url, key)
